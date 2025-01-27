4f2e227a16d4fec2d0450536e3e7c0762230ec61
Erickson Mbuvi <mutisyaerickson@gmail.com>
2025-01-27 14:36:30 +0300

    added usertype to user

--- models/user.py
+++ models/user.py
@@ -5,8 +5,10 @@ from models.base_model import Base, Basemodel
 
 class User(Base, Basemodel):
     """This is the user model"""
+    __tablename__ = 'users'
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     username = Column(String(128), nullable=False)
+    user_type = Column(String(128), nullable=False, default='normal')
